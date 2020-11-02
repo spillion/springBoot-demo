2f0acfa0f6c821f20e281636158b2bd2f3c73383
zhangqianqian <zhangqianqian@kedacom.com>
2020-11-02 11:03:23 +0800

    Revert "Revert "commit code""
    This reverts commit ee16e361

---
+++ src/main/java/com/example/demo/controller/jdbcController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class jdbcController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+    @RequestMapping(value = "/jdbc",method = RequestMethod.POST)
+    public List<Map<String,Object>> userList(HttpServletRequest request, HttpServletResponse response){
+        //    控制跨域
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        String sql="select * from menu";
+        List<Map<String, Object>> list_maps=jdbcTemplate.queryForList(sql);
+        System.out.println(list_maps);
+        return list_maps;
+    }
+
+    @PostMapping("/postname")
+    public String hello(@RequestBody Map params) {
+        String result = "name：" + params.get("name") + "\n age：" + params.get("age");
+        System.out.println(result);
+        return result;
+    }
+}
+
